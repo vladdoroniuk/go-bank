12d853fffed6f311d529c3d0e267c8ac295563ca
vladdoroniuk <vlad.doroniuk.dev@gmail.com>
2023-10-30 22:19:36 +0200

    feat: add /account/{id} route

--- api.go
+++ api.go
@@ -43,6 +43,7 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
 
 	log.Println("JSON API server running on port: ", s.listenAddr)
 	if err := http.ListenAndServe(s.listenAddr, router); err != nil {
@@ -64,7 +65,11 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	id := mux.Vars(r)["id"]
+
+	fmt.Println(id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
