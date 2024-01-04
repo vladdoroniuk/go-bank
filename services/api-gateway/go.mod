077e1d24c33cd17eceecd67c848b278c156cff47
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2024-01-04 14:41:09 +0200

    docs: update readme

--- services/api-gateway/go.mod
+++ services/api-gateway/go.mod
@@ -3,6 +3,8 @@ module github.com/vladdoroniuk/rose/services/api-gateway
 go 1.21.5
 
 require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/gorilla/mux v1.8.1
 	github.com/vladdoroniuk/rose/proto_gen v0.0.0-20231230150022-4174934940d4
 	google.golang.org/grpc v1.60.1
 )
@@ -11,6 +13,7 @@ replace github.com/vladdoroniuk/rose/proto_gen/auth => ../../proto_gen
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,37 +11,40 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	"github.com/caarlos0/env"
+	"github.com/gorilla/mux"
 	pb "github.com/vladdoroniuk/rose/proto_gen/auth"
 )
 
-/* type config struct {
+type config struct {
 	Port string `env:"PORT"`
-} */
+}
 
 func main() {
-	/* cfg := config{}
+	cfg := config{}
 	if err := env.Parse(&cfg); err != nil {
 		log.Panic("Can't parse env")
-	} */
-
-	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	conn, err := grpc.Dial("rose-auth:3001", opts...)
-	if err != nil {
-		log.Panic("Fail to dial")
-	}
-	defer conn.Close()
-
-	client := pb.NewUsersServiceClient(conn)
-	users, err := client.GetUsers(context.Background(), &pb.EmptyRequest{})
-	if err != nil {
-		log.Panic("Can't get users")
 	}
 
-	fmt.Println(users)
+	r := mux.NewRouter()
+	r.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		var opts []grpc.DialOption
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	//log.Printf("Service \"api-gateway\", listening on port: %s\n", cfg.Port)
+		conn, err := grpc.Dial("rose-auth:3001", opts...)
+		if err != nil {
+			log.Panic("Fail to dial")
+		}
+		defer conn.Close()
+
+		client := pb.NewUsersServiceClient(conn)
+		users, err := client.GetUsers(context.Background(), &pb.EmptyRequest{})
+		if err != nil {
+			log.Panic("Can't get users")
+		}
+	})
+
+	log.Printf("Service \"api-gateway\", listening on port: %s\n", cfg.Port)
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
 	<-stop
